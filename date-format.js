b562f0c9f11ff51722009819cd4d95816264ec86
007killer2 <savenko.artem@gmx.de>
2024-10-16 21:10:35 +0200

    Homework 4

---
+++ date-format.js
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+const date = moment().format("DD-MM-YYYY");
+console.log(date);
+
+const date2 = moment().format("MMM Do YY");
+console.log(date2);
+
+const date3 = moment().format("dddd");
+console.log(date3);
---
+++ file-operation.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+const {FILENAME} = process.env;
+
+console.log("File name: ", FILENAME);

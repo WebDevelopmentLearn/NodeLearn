33cc50d28acc3bff800265cd63d77acd9484f3dc
007killer2 <savenko.artem@gmx.de>
2024-12-05 21:22:14 +0100

    Homework 17

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,71 @@
 
 //TODO: Задание 1 START
-function greetUser(username: string): string {
-    return `Привет, ${username}!`;
+const calculateTotal = (price: number, quantity: number, discount: number = 0): number => {
+    return (price * quantity) - (price * quantity * discount / 100);
 }
 
-const message = greetUser("Artem");
-console.log(message);
+console.log(`calculateTotal(100, 2): ${calculateTotal(100, 2)}`); // 200
+console.log(`calculateTotal(100, 2, 10): ${calculateTotal(100, 2, 10)}`); // 180
 //TODO: Задание 1 END
 
 
 //TODO: Задание 2 START
-interface Person {
-    name: string;
-    age: number;
-    city: string;
+let id: string | number;
+const displayId = (id: string | number): string | number => {
+    if (typeof id === 'string') {
+        return id.toUpperCase();
+    } else {
+        return id * 10;
+    }
 }
-
-function printPersonInfo(person: Person): void {
-    console.log(`Имя: ${person.name} \nВозраст: ${person.age} \nГород: ${person.city}`);
-}
-
-const person: Person = {
-    name: "Artem",
-    age: 24,
-    city: "Braunschweig"
-}
-
-printPersonInfo(person);
+console.log(`displayId('abc'): ${displayId('abc')}`); // ABC
+console.log(`displayId(123): ${displayId(123)}`); // 123
 //TODO: Задание 2 END
 
 
 //TODO: Задание 3 START
-function squareNumber(num: number): number {
-    return num * num;
+interface Order {
+    orderId: number,
+    amount: number,
+    status: "pending" | "shipped" | "delivered"
 }
 
-const square = squareNumber(5);
-console.log(square);
+const orders: Order[] = [
+    { orderId: 1, amount: 100, status: "pending" },
+    { orderId: 2, amount: 200, status: "shipped" },
+    { orderId: 3, amount: 300, status: "delivered" },
+    { orderId: 4, amount: 400, status: "pending" },
+    { orderId: 5, amount: 500, status: "shipped" },
+    { orderId: 6, amount: 600, status: "delivered" },
+    { orderId: 7, amount: 700, status: "pending" },
+    { orderId: 8, amount: 800, status: "shipped" },
+    { orderId: 9, amount: 900, status: "delivered" },
+    { orderId: 10, amount: 1000, status: "pending" }
+]
+
+const filterOrdersByStatus = (orders: Order[], status: "pending" | "shipped" | "delivered"): object[] => {
+    return orders.filter(order => order.status === status);
+}
+
+console.log(`filterOrdersByStatus(orders, 'pending'): ${JSON.stringify(filterOrdersByStatus(orders, 'pending'))}`);
 //TODO: Задание 3 END
 
 
 //TODO: Задание 4 START
-function isEven(num: number): boolean {
-    return num % 2 === 0;
+let productInfo: [string, number, number] = ['Apple', 1, 100];
+interface Inventory {
+    name: string,
+    quantity: number,
 }
 
-const even = isEven(5);
-console.log(even);
+const updateStack = (inventory: Inventory, productInfo: [string, number, number]): Inventory => {
+    return {
+        name: productInfo[0],
+        quantity: inventory.quantity + productInfo[1]
+    }
+}
 
-const even2 = isEven(4);
-console.log(even2);
+const inventory = { name: 'Apple', quantity: 10 };
+console.log(`updateStack(inventory, productInfo): ${JSON.stringify(updateStack(inventory, productInfo))}`);
 //TODO: Задание 4 END
 
-
-//TODO: Задание 5 START
-interface Student {
-    name: string;
-    grade: number;
-}
-
-function printStudentInfo(student: Student): void {
-    console.log(`Имя: ${student.name} \nОценка: ${student.grade}`);
-}
-
-const student: Student = {
-    name: "Artem",
-    grade: 5
-}
-
-printStudentInfo(student);
-//TODO: Задание 5 END
-
-
-//TODO: Задание 6 START
-function logMessage(message: string): void {
-    console.log(message);
-}
-
-logMessage("Hello, World!");
-//TODO: Задание 6 END

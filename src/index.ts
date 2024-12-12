3c939dbcb77abd457359174472f71f8fc6fd9633
007killer2 <savenko.artem@gmx.de>
2024-12-12 21:30:44 +0100

    Homework 21

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,56 @@
 console.log("//=============================//");
 console.log("Задание 1");
 
-class Animal {
-    public name: string;
-    public species: string;
-    constructor(name: string, species: string) {
-        this.name = name;
-        this.species = species;
-    }
+abstract class Animal {
+    abstract name: string;
+    abstract species: string;
 
-    sound(): void {
-        console.log("The animal makes a sound");
-    }
+
+    abstract makeSound(): void;
 }
 
 class Dog extends Animal {
     public breed: string;
+    name: string;
+    species: string;
     constructor(name: string, breed: string) {
-        super(name, "Dog");
+        super();
+        this.name = name;
+        this.species = "Dog";
         this.breed = breed;
     }
 
-    sound(): void {
+    makeSound() {
         console.log("The dog barks");
     }
 }
 
-const animal = new Animal("Animal", "Unknown");
-console.log("animal: ", animal);
-animal.sound();
+class Cat extends Animal {
+    public breed: string;
+    name: string;
+    species: string;
+    constructor(name: string, breed: string) {
+        super();
+        this.name = name;
+        this.species = "Cat";
+        this.breed = breed;
+    }
 
-console.log("");
-
+    makeSound() {
+        console.log("The cat meows");
+    }
+}
+const cat = new Cat("Tom", "Siamese");
 const dog = new Dog("Rex", "German Shepherd");
-console.log("dog: ", dog);
-dog.sound();
+
+const animals: Animal[] = [cat, dog];
+
+animals.forEach((animal) => {
+    console.log("animal: ", animal);
+    animal.makeSound();
+    console.log("");
+});
+
 
 console.log("//=============================//");
 //TODO: Задание 1 END
@@ -45,25 +61,63 @@ console.log("//=============================//");
 console.log("//=============================//");
 console.log("Задание 2");
 
-class Library {
-    public static totalBooks: number = 0;
-    constructor() {
-    }
+abstract class Shape {
+    abstract name: string;
 
-    addBook(): void {
-        Library.totalBooks++;
-    }
+    abstract calculateArea(): number;
+
 }
 
-console.log("Library.totalBooks: ", Library.totalBooks);
-for (let i = 0; i < 10; i++) {
-    const library = new Library();
-    library.addBook();
+abstract class ColoredShape extends Shape {
+    abstract color: string;
 }
 
+class ColoredCircle extends ColoredShape {
+    color: string;
+    name: string = "ColoredCircle";
+    radius: number;
 
-console.log("Library.totalBooks: ", Library.totalBooks);
+    constructor(color: string, radius: number) {
+        super();
+        this.color = color;
+        this.radius = radius;
+    }
 
+    calculateArea(): number {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+}
+
+class ColoredRectangle extends ColoredShape {
+    color: string;
+    name: string = "ColoredRectangle";
+    width: number;
+    height: number;
+
+    constructor(color: string, width: number, height: number) {
+        super();
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea(): number {
+        return this.width * this.height;
+    }
+
+}
+
+const coloredCircle = new ColoredCircle("red", 5);
+console.log("coloredCircle", coloredCircle);
+coloredCircle.calculateArea();
+console.log("coloredCircle.calculateArea(): ", coloredCircle.calculateArea());
+
+console.log("");
+
+const coloredRectangle = new ColoredRectangle("blue", 5, 10);
+console.log("coloredRectangle", coloredRectangle);
+console.log("coloredRectangle.calculateArea(): ", coloredRectangle.calculateArea());
 console.log("//=============================//");
 //TODO: Задание 2 END
 
@@ -71,34 +125,149 @@ console.log("//=============================//");
 //TODO: Задание 3 START
 console.log("//=============================//");
 console.log("Задание 3");
-class Vehicle {
-    public make: string;
-    public model: string;
 
-    constructor(make: string, model: string) {
-        this.make = make;
-        this.model = model;
-    }
+abstract class Appliance {
 
+    abstract turnOn(): void;
+    abstract turnOff(): void;
 }
 
-class Motorcycle extends Vehicle {
-    public type: string;
-    constructor(make: string, model: string, type: string) {
-        super(make, model);
-        this.type = type;
+class WashingMachine extends Appliance {
+
+    turnOff(): void {
+        console.log("WashingMachine is disable");
+    }
+
+    turnOn(): void {
+        console.log("WashingMachine is enable");
     }
 }
 
-const vehicle = new Vehicle("Toyota", "Corolla");
-console.log("vehicle: ", vehicle);
+class Refrigerator extends Appliance {
+
+    turnOff(): void {
+        console.log("Refrigerator is disable");
+    }
+
+    turnOn(): void {
+        console.log("Refrigerator is enable");
+    }
+}
+
+const washingMachine = new WashingMachine();
+console.log("washingMachine: ", washingMachine);
+washingMachine.turnOn();
+washingMachine.turnOff();
 
 console.log("");
 
-const motorcycle = new Motorcycle("Yamaha", "FZ6", "Sport");
-console.log("motorcycle: ", motorcycle);
+const refrigerator = new Refrigerator();
+console.log("refrigerator: ", refrigerator);
+refrigerator.turnOn();
+refrigerator.turnOff();
 
 console.log("//=============================//");
 //TODO: Задание 3 END
 
 
+//TODO: Задание 4 START
+console.log("//=============================//");
+console.log("Задание 4");
+
+abstract class Account {
+
+    abstract deposit(amount: number): number;
+
+    abstract withdraw(amount: number): number;
+
+}
+
+class SavingsAccount extends Account {
+    private balance: number;
+    constructor(balance: number) {
+        super();
+        this.balance = balance;
+    }
+    deposit(amount: number): number {
+        return this.balance += amount;
+    }
+
+    withdraw(amount: number): number {
+        return this.balance -= amount;
+    }
+
+}
+
+
+class CheckingAccount extends Account {
+    private balance: number;
+    constructor(balance: number) {
+        super();
+        this.balance = balance;
+    }
+
+    deposit(amount: number): number {
+        return this.balance += amount;
+    }
+
+    withdraw(amount: number): number {
+        return this.balance -= amount;
+    }
+}
+
+const savingsAccount = new SavingsAccount(1000);
+console.log("savingsAccount: ", savingsAccount);
+console.log("savingsAccount.deposit(10): ", savingsAccount.deposit(10));
+console.log("savingsAccount.withdraw(50): ", savingsAccount.withdraw(50));
+
+console.log("");
+
+const checkingAccount = new CheckingAccount(1000);
+console.log("checkingAccount: ", checkingAccount);
+console.log("checkingAccount.deposit(10): ", checkingAccount.deposit(10));
+console.log("checkingAccount.withdraw(50): ", checkingAccount.withdraw(50));
+
+console.log("//=============================//");
+//TODO: Задание 4 END
+
+
+
+
+//TODO: Задание 5 START
+console.log("//=============================//");
+console.log("Задание 5");
+
+abstract class Media {
+
+    abstract play(): void;
+
+}
+
+
+class MyAudio extends Media {
+
+    play(): void {
+        console.log("Playing audio");
+    }
+
+}
+
+class Video extends Media {
+
+    play(): void {
+        console.log("Playing video");
+    }
+
+}
+
+const media: Media[] = [new MyAudio(), new Video()];
+
+media.forEach((obj) => {
+    console.log("media: ", obj);
+    obj.play();
+    console.log("");
+})
+
+
+console.log("//=============================//");
+//TODO: Задание 5 END

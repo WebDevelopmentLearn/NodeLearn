b31d272cf1d30235a467bc7fc209b475c624750d
007killer2 <savenko.artem@gmx.de>
2024-12-18 16:12:07 +0100

    Homework 23

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,60 @@
-import {capitalize, reverse} from "./stringUtils";
-import Finance from "./finance";
-import UserManagement from "./userManagement";
-import {generateFibonacci, generatePrimeNumbers} from "./sequenceUtils";
+
 
 //TODO: Задание 1 START
 console.log("//=============================//");
 console.log("Задание 1");
 
-console.log("capitalize('hello'): ", capitalize('hello')); // Hello
-console.log("reverse('hello'): ", reverse('hello')); // olleh
+const firstAsyncFunc = async(): Promise<string> => {
+    console.log("Задание 1: firstAsyncFunc");
+    return new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Задание 1: firstPromise");
+        }, 1000);
+    });
+}
+
+const secondAsyncFunc = async(): Promise<string> => {
+    console.log("Задание 1: secondAsyncFunc");
+    return new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Задание 1: secondPromise");
+        }, 2000);
+    });
+}
+
+const thirdAsyncFunc = async(): Promise<string> => {
+    console.log("Задание 1: thirdAsyncFunc");
+    return new Promise<string>((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Задание 1: thirdPromise");
+        }, 3000);
+    });
+}
+
+const asyncFunc = async() => {
+    try {
+        const result1 = await firstAsyncFunc();
+        console.log("result1: ", result1);
+
+        const result2 = await secondAsyncFunc();
+        console.log("result2: ", result2);
+
+        const result3 = await thirdAsyncFunc();
+        console.log("result3: ", result3);
+
+    } catch (error) {
+        console.error("Ошибка при обработке промисов в задании 1: ", error);
+    }
+}
+
+const resultAsyncFunc = asyncFunc().then(() => {
+    console.log("Задание 1 выполнено успешно");
+}).catch((error) => {
+    console.error("Ошибка при выполнении задания 1: ", error);
+}).finally(() => {
+    console.log("Задание 1 завершено");
+});
+console.log("asyncFunc(): ", resultAsyncFunc);
 
 console.log("//=============================//");
 //TODO: Задание 1 END
@@ -18,11 +64,33 @@ console.log("//=============================//");
 console.log("//=============================//");
 console.log("Задание 2");
 
-const loanCalculator = new Finance.LoanCalculator(100000, 10, 5);
-console.log("loanCalculator.calculateMonthlyPayment(): ", loanCalculator.calculateMonthlyPayment()); // 2124.02
+const asyncFunc2 = async(arr: string[]) => {
+    const promisesArr: Promise<string>[] = [];
+    for (const item of arr) {
+        const result = await new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                resolve(item.toUpperCase());
+            }, 1000);
+        });
+        console.log(result);
+        promisesArr.push(Promise.resolve(result));
+    }
 
-const taxCalculator = new Finance.TaxCalculator(60000);
-console.log("taxCalculator.calculateTax(): ", taxCalculator.calculateTax()); // 6500
+    const results = await Promise.all(promisesArr);
+    console.log("Results: ", results);
+    return results;
+}
+
+const arr = ["one", "two", "three"];
+
+const resultAsyncFunc2 = asyncFunc2(arr).then(() => {
+    console.log("Задание 2 выполнено успешно");
+}).catch((error) => {
+    console.error("Ошибка при выполнении задания 2: ", error);
+}).finally(() => {
+    console.log("Задание 3 завершено");
+});
+console.log("asyncFunc2(): ", resultAsyncFunc2);
 
 console.log("//=============================//");
 //TODO: Задание 2 END
@@ -32,11 +100,42 @@ console.log("//=============================//");
 console.log("//=============================//");
 console.log("Задание 3");
 
-const adminUser = new UserManagement.Admin.AdminUser("John", "john_doe@gmail.com", false);
-adminUser.toggleSuperAdminStatus();
-console.log("adminUser: ", adminUser);
-adminUser.toggleSuperAdminStatus();
-console.log("adminUser: ", adminUser);
+const asyncFunc3 = async() => {
+    try {
+        const firstPromise = new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Задание 3: firstPromise");
+            }, 1000);
+        });
+
+        const secondPromise = new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                resolve("Задание 3: secondPromise");
+            }, 2000);
+        });
+
+        const thirdPromise = new Promise<string>((resolve, reject) => {
+            setTimeout(() => {
+                reject("Задание 3: thirdPromise");
+            }, 3000);
+        });
+
+       const results = await Promise.all([firstPromise, secondPromise, thirdPromise]);
+       console.log("Results: ", results);
+    } catch (error) {
+        console.error("Ошибка при обработке промисов в задании 3: ", error);
+    }
+
+}
+
+const resultAsyncFunc3 = asyncFunc3().then(() => {
+    console.log("Задание 3 выполнено успешно");
+}).catch((error) => {
+    console.error("Ошибка при выполнении задания 3: ", error);
+}).finally(() => {
+    console.log("Задание 3 завершено");
+});
+console.log("asyncFunc3(): ", resultAsyncFunc3);
 
 console.log("//=============================//");
 //TODO: Задание 3 END
@@ -46,11 +145,34 @@ console.log("//=============================//");
 console.log("//=============================//");
 console.log("Задание 4");
 
-console.log("generateFibonacci(5): ", generateFibonacci(5)); // [1, 1, 2, 3, 5]
-console.log("generatePrimeNumbers(5): ", generatePrimeNumbers(5)); // [2, 3, 5, 7, 11]
+const asyncFunc4 = async(arr: number[]) => {
+    const promisesArr: Promise<number>[] = [];
+    for (const item of arr) {
+        const result = await new Promise<number>((resolve, reject) => {
+            setTimeout(() => {
+                resolve(item);
+            }, item);
+        });
+        console.log(result);
+        promisesArr.push(Promise.resolve(result));
+    }
+
+    const results = await Promise.all(promisesArr);
+    console.log("Results: ", results);
+    return results;
+};
+
+const arr4 = [1000, 2000, 3000];
+
+
+const resultAsyncFunc4 = asyncFunc4(arr4).then(() => {
+    console.log("Задание 4 выполнено успешно");
+}).catch((error) => {
+    console.error("Ошибка при выполнении задания 4: ", error);
+}).finally(() => {
+    console.log("Задание 4 завершено");
+});
+console.log("asyncFunc4(arr4): ", resultAsyncFunc4);
 
 console.log("//=============================//");
 //TODO: Задание 4 END
-
-
-
--- src/stringUtils.ts
+++
@@ -1,4 +0,0 @@
-
-export const capitalize = (s: string): string => s.charAt(0).toUpperCase() + s.slice(1);
-
-export const reverse = (s: string): string => s.split("").reverse().join("");
